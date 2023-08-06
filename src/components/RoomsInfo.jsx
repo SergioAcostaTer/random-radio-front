ecabd44050d04aed88b264ec86e6aa7e1244cdb2
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-08-06 20:55:00 +0100

    6 rooms

--- src/components/RoomsInfo.jsx
+++ src/components/RoomsInfo.jsx
@@ -10,6 +10,9 @@ const RoomsInfo = ({ audio }) => {
       <RoomInfo audio={audio} room={"room1"} />
       <RoomInfo audio={audio} room={"room2"} />
       <RoomInfo audio={audio} room={"room3"} />
+      <RoomInfo audio={audio} room={"room4"} />
+      <RoomInfo audio={audio} room={"room5"} />
+      <RoomInfo audio={audio} room={"room6"} />
 
 
     </>
