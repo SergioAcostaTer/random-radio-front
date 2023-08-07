ce00d53a2456f1c66dd6b7c65c784ed601b74ded
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-08-07 22:46:57 +0100

    embed audio

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -3,53 +3,29 @@ import { useParams } from "react-router-dom";
 import io from "socket.io-client";
 import Chat from "../components/Chat";
 import RoomsInfo from "../components/RoomsInfo";
-import axios from "axios";
 
 function Room() {
     const { room } = useParams();
     const [sockett, setSocket] = useState(null);
     const [users, setUsers] = useState([]);
-
-
-
-
+    
     // State to store users, chat messages, and current song details
     const [data, setData] = useState(null);
     const [audioElement, setAudioElement] = useState(null);
-
-
-
+    
     useEffect(() => {
-        // const socket = io("http://192.168.1.133:4000/" + room); // Replace with your room name
         const socket = io("https://random-radio-back.onrender.com/" + room); // Replace with your room name
-        // const socket = io("http://192.168.0.30:4000/" + room); // Replace with your room name
         setSocket(socket);
-
-        const handleSongDetails = async (data) => {
+        
+        const handleSongDetails = (data) => {
             console.log(data);
             setData(data);
-        
+            
             try {
-                // Make a fetch request to the URL to check its status
-                const response = await axios.head(data.url, {
-                    method: "GET",
-                    mode: "no-cors",
-                });
-
-                console.log(response);
-        
-                // Check if the status code is 403 Forbidden
-                if (response.status === 403) {
-                    console.log("URL returned a 403 status code. Skipping song...");
-                    socket.emit("skipSong", data);
-                    return;
-                }
-        
-                // Create an audio element and set its source to the provided URL
-                const audio = new Audio(data.url);
+                const audio = new Audio(); // Create an empty audio element
+                audio.src = data.url; // Set the source URL
                 setAudioElement(audio);
-        
-                // Set the current time of the audio and play it
+                
                 audio.currentTime = data.currentTime;
                 audio.play();
             } catch (err) {
@@ -58,20 +34,16 @@ function Room() {
             }
         };
         
-
-        socket.emit("joinRoom")
-
+        socket.emit("joinRoom");
+        
         socket.on("userCount", (users) => {
             setUsers(users);
         });
-
-
-
+        
         socket.on("songDetails", (song) => handleSongDetails(song));
-
-        // Clean up on unmount
+        
         return () => {
-            socket.emit("leaveRoom")
+            socket.emit("leaveRoom");
             socket.off("songDetails", handleSongDetails);
             if (audioElement) {
                 audioElement.pause();
@@ -79,9 +51,8 @@ function Room() {
             }
             socket.disconnect();
         };
-    }, [room]);
-
-
+    }, [room, audioElement]);
+    
     return (
         <>
             <div className="h-full w-full relative">
@@ -92,14 +63,9 @@ function Room() {
                     <h1>Now Playing: {data?.name} - {data?.artists[0]}</h1>
                     <h2>Users in room: {users}</h2>
                 </div>
-
-                {
-                    sockett
-                        ? <Chat socket={sockett} />
-                        : null
-                }
+                
+                {sockett ? <Chat socket={sockett} /> : null}
             </div>
-
         </>
     );
 }
