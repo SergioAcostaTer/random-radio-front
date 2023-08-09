303b9a9ae80854207e9df065f338932e2ec50cf3
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-08-09 23:48:06 +0100

    chat

--- src/components/RoomInfo.jsx
+++ src/components/RoomInfo.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import io from "socket.io-client";
 import getContrastColor from "../services/getContrastColor";
 import "../styles/RoomInfo.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function RoomInfo({ roomInfo, refreshDeleteChat }) {
     const [data, setData] = useState(null);
@@ -10,14 +12,91 @@ function RoomInfo({ roomInfo, refreshDeleteChat }) {
     const [currentTime, setCurrentTime] = useState(0); // Current time state
     const navigate = useNavigate();
 
+
+
+
     useEffect(() => {
         // const socket = io("http://localhost:3000/" + roomInfo.roomName);
         const socket = io("https://random-radio-back.onrender.com/" + roomInfo.roomName);
+
+        let trueAfter5seconds = false
+
+        setTimeout(() => {
+            trueAfter5seconds = true
+        }, 5000);
+
 
         const handleSongDetails = (song) => {
             setData(song);
             setCurrentTime(song.currentTime);
             console.log(song.currentTime);
+
+            //10% of probability of changing the song
+            if (Math.random() < 1 && trueAfter5seconds) {
+                const toastId = toast.info(
+                    <div
+                        className={`custom-toast`} // Add the custom CSS class
+                        style={{
+                            backgroundColor: song.colors[0].hex,
+                        }}
+                    >
+                        <div className="info-cont">
+                            <p style={{color: getContrastColor(song.colors[0].hex)[0]}} className="playlist">Wanna change of room?</p>
+                            <div className="song-details">
+                                <img src={song.cover} alt={song.name} />
+                                <div className="text">
+                                    <p style={{color: getContrastColor(song.colors[0].hex)[0]}}  className="song-title">{song.name}</p>
+                                    <p style={{color: getContrastColor(song.colors[0].hex)[0]}} className="song-author">{song.artists[0]}</p>
+                                </div>
+                                <div className="buttons">
+                                    <button className="toast-button" onClick={() => {
+                                        navigate(`/${roomInfo.roomName}`)
+                                        toast.dismiss(toastId)
+                                    }}
+                                    ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                            <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+                                        </svg></button>
+                                    <button className="toast-button-no" onClick={() => {
+                                        toast.dismiss(toastId)
+                                    }}
+                                    ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                                        </svg></button>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>,
+                    {
+                        position: "bottom-center",
+                        autoClose: 7000,
+                        closeButton: false,
+                        //no icon
+                        icon: false,
+                        style: {
+                            backgroundColor: "transparent",
+                            color: "transparent",
+                            boxShadow: "none",
+                            width: "100%",
+                            height: "100%",
+                            padding: "0",
+                            margin: "0",
+                            borderRadius: "0",
+                            border: "none",
+                            fontSize: "0",
+                        },
+                        progressStyle: {
+                            // backgroundColor: song.colors[0].hex,
+                            backgroundColor: "transparent",
+                        }
+                    }
+                );
+
+
+
+
+            }
+
         };
 
         socket.on("songDetails", handleSongDetails);
@@ -47,6 +126,7 @@ function RoomInfo({ roomInfo, refreshDeleteChat }) {
 
     return (
         <>
+            {/* toast */}
             {data ? (
                 <div onClick={() => {
                     refreshDeleteChat();
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { ToastContainer } from 'react-toastify'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
+    <ToastContainer/>
         <App />
     </BrowserRouter>
 )
